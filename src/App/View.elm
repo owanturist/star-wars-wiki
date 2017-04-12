dea4b72b43a78137b2f4a6604b95fad5dcfd702a
Anton Ovechkin <owanturist@gmail.com>
2017-04-12 22:00:55 +0700

    Put page at container

--- src/App/View.elm
+++ src/App/View.elm
@@ -1,15 +1,23 @@
 module View exposing (view)
 
-import Html exposing (Html, div, text)
-import Types.Msg exposing (Msg(..))
-import Types.Model exposing (Model)
-import Routes exposing (Route(..))
-import Homepage.View
 import Counter.View
+import Homepage.View
+import Html exposing (Html, div, text)
+import Html.Attributes exposing (class)
+import Routes exposing (Route(..))
+import Types.Model exposing (Model)
+import Types.Msg exposing (Msg(..))
 
 
 view : Model -> Html Msg
-view { route, ui } =
+view model =
+    div [ class "container" ]
+        [ viewRoute model
+        ]
+
+
+viewRoute : Model -> Html Msg
+viewRoute { route, ui } =
     case route of
         Homepage ->
             Homepage.View.view
